77396b6560a181f67a5d7659bcb635347f4b8dab
Alejandro Sanchez Ferrer <asanc.tech@gmail.com>
2024-01-09 21:08:41 +0100

    Fixed issues related to demo inference

--- demo.py
+++ demo.py
@@ -10,6 +10,9 @@ import warnings
 import cv2
 import tqdm
 
+import sys
+sys.path.append('./detectron2') 
+
 from detectron2.config import get_cfg
 from detectron2.data.detection_utils import read_image
 from detectron2.utils.logger import setup_logger
